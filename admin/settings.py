9d9f95bce05ca876249e04615ea2516a5371c385
lanjithkumar15 <lanjithkumar15@localhost.localdomain>
2024-02-26 13:35:19 +0530

    push the data

--- admin/settings.py
+++ admin/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'admin.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'calculate_db',
-        'USER': 'postgres',
-        'PASSWORD': 'lanjith15',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
     }
 }
 
